524212724228439118d2dc1bb89a217b2d712425
ParalkarSakshi <sakshiparalkar1@gmail.com>
2024-01-24 21:08:42 -0800

    binding

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Binding';
+
+  // image binding
+  // imageUrl="../assets/image/image1.jfif";
+
+  // button binding
+  // off=false;
+  // off=true;
+
+  // onClick(){
+  //   console.log("The button is clicked");
+  // }
+
+  // tag binding
+  //  isError=true;
+      isError=false;  
+}
